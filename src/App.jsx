5bb4756c2b227eaf5f6a12d660d9f0beb7bb085e
Sreesairam V <sairam2381@gmail.com>
2026-02-14 14:20:03 +0530

    The things are added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HeroPageComponent from "./home-page/hero-page/hero-page";
+
+function App() {
+  return (
+    <>
+      <HeroPageComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/home-page/hero-page/hero-page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HeroPageComponent = () => {
+  return (
+    <div>
+      <h1>Hello world 33343</h1>
+    </div>
+  );
+};
+export default HeroPageComponent;
