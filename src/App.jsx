53f8129215a57adb07df46b8078769e411aee633
Sreesairam V <sairam2381@gmail.com>
2026-02-14 14:40:13 +0530

    component pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import HeroPageComponent from "./home-page/hero-page/hero-page";
+import SymbolOfExcellenceComponent from "./home-page/symbol-of-excellence/symbol-of-excellence";
 
 function App() {
   return (
     <>
       <HeroPageComponent />
+      <SymbolOfExcellenceComponent />
     </>
   );
 }
---
+++ src/home-page/symbol-of-excellence/symbol-of-excellence.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SymbolOfExcellenceComponent = () => {
+  return <h1>Symbol of Excellence</h1>;
+};
+export default SymbolOfExcellenceComponent;
